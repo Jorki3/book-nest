9c55576d412c08cf7b13b627b9d40788311c3802
Jorki3 <eduardomttzt01@outlook.com>
2025-06-03 23:15:10 -0600

    UserState

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
   import { invalidate } from "$app/navigation";
   import { Header } from "$components";
+  import { setUserState } from "$lib/state/user-state.svelte";
   import "./../app.css";
 
   let { data, children } = $props();
-  let { session, supabase } = $derived(data);
+  let { session, supabase, user } = $derived(data);
+
+  let userSate = setUserState({
+    session: data.session,
+    supabase: data.supabase,
+    user: data.user,
+  });
+
+  $effect(() => {
+    userSate.updateState({ session, supabase, user });
+  });
 
   $effect(() => {
     const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
